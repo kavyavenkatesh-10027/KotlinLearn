c71d036de1e49ff9193909e6b68e40e58eb236f6
kavyavenkatesh-10027 <kavyavenkatesh2007@gmail.com>
2026-06-11 15:01:40 +0530

    Learning kotlin

---
+++ src/Nullablility.kt
@@ -0,0 +1,21 @@
+
+    fun main(){
+        println("Please enter a number:")
+        val input = readln()
+        val inputAsInt = input.toIntOrNull()
+
+        println("You entered $inputAsInt")
+        operators()
+    }
+
+    fun operators(){
+        println("Please enter another number:")
+        val value = readln().toIntOrNull() ?: 0 // ?: is used to assign a default value if value becomes null
+
+        println("Is value even? ${value%2 == 0}") // HERE, in case of invalid input, value become 0 and always prints true (since zero is even_number)
+
+        println("Deposit money to increase cash by a rupee: ")
+        val cash = readln().toIntOrNull() ?. inc() // null gets assigned to the value if the precedent of ? is null, without crashing the method chaining
+        println("Your current cash balance is $cash")
+
+    }
